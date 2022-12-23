a564286dd70a2ce8e80e49d70fae4731bf7ed1e0
Bribak <daniel@bojar.net>
2022-12-23 08:31:08 +0100

    restrict positive adducts to intact M

--- CandyCrunch/analysis.py
+++ CandyCrunch/analysis.py
@@ -587,6 +587,7 @@ def subgraphs_to_domon_costello(nx_mono,subgs, max_frags = 3):
       cut_labels = ['M']
     if len(cut_labels) <= max_frags:
       ion_names.append((cut_labels))
+  ion_names = [k for k in ion_names if not (('M_+Acetate' in k) and (len(k) > 1))]
 
   return sorted(ion_names, key = len)[:5]
 
@@ -604,9 +605,11 @@ def get_most_likely_fragments(out_in):
   single_list = []
   for t in out[::-1]:
     out_len = len(out_list)
+    #check for any single-fragment occurrences
     if len(t) > 1 and isinstance(t[1], list) and len(t[1][:1]) == 1:
       out_list.append((t[0], t[1][0]))
       single_list.append(t[1][0][:1])
+    #prioritize double-fragments with observed single-fragments
     elif len(t) > 1 and isinstance(t[1], list) and len(t[1][:1]) == 2:
       for tt in t[1]:
         if len(tt) == 2 and any([k in tt for k in single_list]):
@@ -675,6 +678,8 @@ def CandyCrumbs(glycan_string,fragment_masses,end_threshold, libr = None,
   """
   if libr is None:
     libr = lib
+  if end_threshold < start:
+    start = end_threshold/2
   hit_list = []
   fragment_masses = sorted(fragment_masses)
   attribute_dict = refine_global_mods(glycan_string)
