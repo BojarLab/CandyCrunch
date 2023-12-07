5404f36361628c7561cef2168971f73d9034210c
urbj <urbangjames29@gmail.com>
2023-12-07 14:46:39 +0100

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="CandyCrunch",
-    version="0.2.1",
+    version="0.2.2",
     author="Daniel Bojar",
     author_email="daniel.bojar@gu.se",
     description="Package for predicting glycan structure from LC-MS/MS data",
