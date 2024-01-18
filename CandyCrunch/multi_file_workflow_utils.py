f252d0da0820cce1031e7eb7df6e979363abc5dd
urbj <urbangjames29@gmail.com>
2024-01-18 10:42:48 +0100

    Added new mass grouping and harmonisation functions

--- CandyCrunch/multi_file_workflow_utils.py
+++ CandyCrunch/multi_file_workflow_utils.py
@@ -1,10 +1,11 @@
 from collections import Counter
 import itertools
+import ast
 import numpy as np
 import pandas as pd
 from glycowork.motif.processing import enforce_class
-from glycowork.motif.tokenization import composition_to_mass
-from prediction import average_dicts,mass_check,process_for_inference,get_topk,average_preds
+from glycowork.motif.tokenization import composition_to_mass,glycan_to_composition
+from prediction import average_dicts,mass_check,process_for_inference,get_topk,average_preds,domain_filter,bin_intensities
 from prediction import candycrunch,glycans,temperature
 import matplotlib.pyplot as plt
 
@@ -19,8 +20,9 @@ def calculate_candidate_clusters(sample,categories,inter_sample_thresh):
     for cluster in sample:
         candidate_categories = set()
         for category,cat_pop in categories.items():
-            if abs(np.mean(cluster)-np.mean([p for q in cat_pop for p in q])) < inter_sample_thresh:
-                candidate_categories.add(category)
+            if cat_pop:
+                if abs(np.mean(cluster)-np.mean([p for q in cat_pop for p in q])) < inter_sample_thresh:
+                    candidate_categories.add(category)
         all_candidate_categories.append(candidate_categories)
     return all_candidate_categories
 
@@ -158,9 +160,43 @@ def assign_RT_group(single_mass_df,RT_gap):
     single_mass_df = single_mass_df.assign(RT_group = single_mass_df['RT_group'].cumsum())
     return single_mass_df
 
-def assign_mass_groups(all_ms2_spectra):
-    all_ms2_spectra = all_ms2_spectra.assign(mass_group = all_ms2_spectra.reducing_mass.round(0))
-    return all_ms2_spectra
+# def assign_mass_groups(all_ms2_spectra):
+#     all_ms2_spectra = all_ms2_spectra.assign(mass_group = all_ms2_spectra.reducing_mass.round(0))
+#     return all_ms2_spectra
+
+def assign_mass_groups(loaded_ms2_spectra,mass_window,min_spectra):
+    mass_groups = create_mass_groups(loaded_ms2_spectra,mass_window,min_spectra)
+    for x in mass_groups:
+        loaded_ms2_spectra.loc[(loaded_ms2_spectra.reducing_mass>x-mass_window)&(loaded_ms2_spectra.reducing_mass<x+mass_window),'mass_label'] = x
+    return loaded_ms2_spectra
+
+def assign_dict_labels_list(listy,break_dist=0.5):
+  k = 0
+  labels = [0]
+  for d in range(1, len(listy)):
+    if abs(listy[d-1] - listy[d]) < break_dist:
+      labels.append(k)
+    else:
+      k += 1
+      labels.append(k)
+  return labels
+
+def repeated_discontinuity_split(list_to_split,max_group_size,starting_break_dist,all_groups = []):
+    if np.ptp(list_to_split)<max_group_size:
+        all_groups.append(list_to_split)
+        return None
+    dict_labels = assign_dict_labels_list(list_to_split,break_dist=starting_break_dist)
+    all_labelled_masses = pd.DataFrame.from_dict(list(zip(dict_labels,list_to_split)))
+    all_labelled_masses.columns=['label','mass']
+    mass_groups = all_labelled_masses.groupby('label').agg(list)['mass'].tolist()
+    for cat in mass_groups:
+            repeated_discontinuity_split(cat,max_group_size,starting_break_dist=starting_break_dist*0.95,all_groups = all_groups)
+    return all_groups
+
+def create_mass_groups_repeated_split(sorted_masses,max_group_size,starting_break_dist):
+    mass_groups = repeated_discontinuity_split(sorted_masses,max_group_size,starting_break_dist,all_groups=[])
+    mass_groups_means = [[y]*len(x) for x,y in zip(mass_groups,[np.mean(x) for x in mass_groups])]
+    return [x for y in mass_groups_means for x in y]
 
 def generate_basic_human_masses(max_monosaccharides):
     #Could probably replace this with a sugarbase search
@@ -169,12 +205,8 @@ def generate_basic_human_masses(max_monosaccharides):
         masses_list.extend([(composition_to_mass(Counter(x))+1,x) for x in itertools.combinations_with_replacement(['Hex','HexNAc','Neu5Ac','dHex','Neu5Gc'],i)])
     return masses_list
 
-def assign_categories(all_ms2_spectra,filename_label_map,masses = None):
+def assign_categories(all_ms2_spectra,filename_label_map,search_masses):
     all_mass_dfs = []
-    if not masses:
-        search_masses = all_ms2_spectra.reducing_mass.round(0).unique()
-    else:
-        search_masses = masses
     for search_mass in search_masses:
         mass_group_dfs = []
         for condition_label in sorted({x[0] for x in filename_label_map.values()}):
@@ -218,8 +250,9 @@ def group_category_spectra(single_mass_df):
     return single_mass_df
 
 def generate_valid_predictions(single_mass_df,preds_out,glycan_class,modification='reduced',mode='negative'):
+    class_map = {0:'O',1:'N',2:'free'}
     candidates = [[y for y in preds_out[idx][:5] if mass_check(mass, y, modification = modification, mode = mode)] for mass,idx in zip(single_mass_df.reducing_mass,single_mass_df.index)]
-    candidates = [[y for y in x if enforce_class(y,glycan_class)] for x in candidates]
+    candidates = [[y for y in x if enforce_class(y,class_map[glycan_class])] for x in candidates]
     return candidates
 
 def select_unique_predictions_by_abundance(candidate_predictions):
@@ -239,25 +272,24 @@ def grouped_df_to_preds(grouped_df,glycan_class):
     preds, pred_conf = average_preds(preds, pred_conf)
     return preds,pred_conf
 
-def filter_grouped_df_predictions(grouped_df,preds):
+def filter_grouped_df_predictions(grouped_df,preds,glycan_class):
     filtered_preds_df = grouped_df.copy(deep=True)
     filtered_preds_df = filtered_preds_df.reset_index()
     filtered_preds_df = filtered_preds_df[(filtered_preds_df.RT>5)&(filtered_preds_df.RT<40)]
     filtered_preds_df = filtered_preds_df.sort_values('num_spectra',ascending=False)
-    valid_predictions = generate_valid_predictions(filtered_preds_df,preds,'O')
+    valid_predictions = generate_valid_predictions(filtered_preds_df,preds,glycan_class)
     unique_predictions = select_unique_predictions_by_abundance(valid_predictions)
     filtered_preds_df['final_predictions'] = unique_predictions
     filtered_preds_df = filtered_preds_df[~filtered_preds_df.final_predictions.isnull()]
     return filtered_preds_df
 
-def categories_df_to_unique_predictions(df_out_categories,masses):
+def categories_df_to_unique_predictions(df_out_categories,glycan_class,search_masses):
     all_filtered_dfs = []
-    for search_mass in masses:
+    for search_mass in search_masses:
         single_mass_df = df_out_categories[df_out_categories.mass_label == search_mass].copy(deep=True)
-        print(len(single_mass_df))
         grouped_df = group_category_spectra(single_mass_df)
-        p1,c1 = grouped_df_to_preds(grouped_df,0)
-        filtered_grouped_df = filter_grouped_df_predictions(grouped_df,p1)
+        p1,c1 = grouped_df_to_preds(grouped_df,glycan_class)
+        filtered_grouped_df = filter_grouped_df_predictions(grouped_df,p1,glycan_class)
         all_filtered_dfs.append(filtered_grouped_df)
     return pd.concat(all_filtered_dfs,ignore_index=True)
 
@@ -273,6 +305,47 @@ def concat_to_annotation_table(concat_df,label_column,intensity_column):
     ant_table = ant_table.set_index('cols').T
     ant_table.index.name = 'Glycan'
     return ant_table.reset_index()
+
+def map_predictions_to_spectra(df_out,df_out_unique_preds):
+    label_to_pred_map = {(ml,cl):fp for ml,cl,fp in zip(df_out_unique_preds.mass_label,df_out_unique_preds.category_label,df_out_unique_preds.final_predictions)}
+    df_out['final_pred'] = [label_to_pred_map[(ml,cl)] if (ml,cl) in label_to_pred_map else None for ml,cl in zip(df_out.mass_label,df_out.category_label)]
+    return df_out
+
+def wrap_inference_multi_file(spectra_directory,filename_label_map,glycan_class,max_group_size,starting_break_dist,top_n_masses=None):
+    loaded_ms2_spectra = load_ms2_spectra(spectra_directory,filename_label_map)
+    loaded_ms2_spectra = loaded_ms2_spectra.sort_values('reducing_mass')
+    loaded_ms2_spectra['mass_label'] = create_mass_groups_repeated_split(loaded_ms2_spectra.reducing_mass,max_group_size,starting_break_dist)
+    sorted_mass_labels = loaded_ms2_spectra.groupby('mass_label').count().sort_values('peak_d',ascending=False).index.tolist()
+    if top_n_masses:    
+        sorted_mass_labels = sorted_mass_labels[:top_n_masses]
+    df_out = assign_categories(loaded_ms2_spectra,filename_label_map,sorted_mass_labels)
+    df_out = df_out.assign(peak_d = [ast.literal_eval(x) for x in df_out.peak_d])
+    df_out_unique = categories_df_to_unique_predictions(df_out,glycan_class,sorted_mass_labels)
+    df_out = map_predictions_to_spectra(df_out,df_out_unique)
+    return df_out,df_out_unique
+
+step = (3000 - 39.714) / (2048 - 1)
+frames = np.array([39.714 + step * i for i in range(2048)])
+def get_top_frags(peak_d):
+    actual_top_peaks = frames[np.argsort(bin_intensities(peak_d,frames)[0])[-10:][::-1]]+step/2
+    return actual_top_peaks
+
+def harmonise_df_out_unique(df_out_unique):
+    df_out_harm = df_out_unique.copy(deep=True)
+    df_out_harm['composition'] = [glycan_to_composition(g) for g in df_out_harm.final_predictions]
+    df_out_harm['charge'] = [round(composition_to_mass(df_out_harm.composition.values.tolist()[k])/df_out_harm.reducing_mass.values.tolist()[k])*-1 for k in range(len(df_out_harm))]
+    df_out_harm['predictions'] = [[(g,0.99)] for g in df_out_harm.final_predictions]
+    df_out_harm['top_fragments'] = [get_top_frags(k) for k in df_out_harm.peak_d]
+    df_out_harm = df_out_harm[['predictions','final_predictions','composition','num_spectra',	'charge','RT','peak_d','top_fragments','reducing_mass','category_label','mass_label']]
+    return df_out_harm
+
+def run_domain_filter(df_in_unique):
+    df_out_unique = df_in_unique.copy(deep=True)
+    df_out_unique = df_out_unique.set_index('reducing_mass')
+    df_out_unique = domain_filter(df_out_unique,'O')
+    df_out_unique['string_predictions'] = [str(x) for x in df_out_unique.predictions]
+    df_out_unique = df_out_unique[df_out_unique.string_predictions.str.len()>3]
+    return df_out_unique                           
 
 def get_glycan_plot_df(annotation_table,species_name):
     plot_df = annotation_table[annotation_table.Glycan == species_name].T.reset_index()
